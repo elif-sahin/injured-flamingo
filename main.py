fce45dc494ccfbc04c0c72d06287d9f242d03468
aemreb <44240521+aemreb@users.noreply.github.com>
2019-04-11 16:35:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,52 @@
 
 
 import vlc
+import os
+import time
+import _tkinter
+import tkinter as tk
+from tkinter import filedialog
 
-class Funct:
-    def playmp3(self):
-        p = vlc.MediaPlayer("/Users/emre/balance.mp3")
-        p.play()
-        a = p.play()
-        print(a)
+def chooseFile():
+    root = tk.Tk()
+    root.withdraw()
+    file_path = filedialog.askopenfilename()
+    root.update()
+    return file_path
 
-        print(p.is_playing())
+def playMusic():
+    file_path = chooseFile()
+    p = vlc.MediaPlayer(file_path)
 
+    p.play()
 
-    def playwav(self):
-        p = vlc.MediaPlayer("/Users/emre/bass.wav")
-        p.play()
+    while True:
+        pass
 
-        while p.is_playing():
-            pass
+def playVideo():
+
+    file_path = chooseFile()
+
+    i = vlc.Instance('--verbose 2'.split())
+    p = i.media_player_new()
+    p.set_mrl(file_path)
+    p.play()
+
 
 
 def main():
-    print("1: Play mp3 \n")
-    print("2: Play wav \n")
+    print("1: Play Music \n")
+    print("2: Play Video \n")
 
     switch = input()
 
     if switch == "1":
-        Funct.playmp3(Funct)
+        playMusic()
 
-    elif switch == "2":
-        Funct.playwav(Funct)
+    if switch == "2":
+        playVideo()
+
+
 
 
 
